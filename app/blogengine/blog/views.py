ad8b2126400d5bdbccf99eb91c2b0d088875f234
Igor Permyakov <imouttasightx@gmail.com>
2020-05-09 18:00:41 +0600

    Made a route to 'blog' app, added base templates

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def posts_list(request):
+    posts = ['One', 'Two', 'Three', 'Four', 'Five']
+    return render(request, 'blog/index.html', context={'posts': posts})
