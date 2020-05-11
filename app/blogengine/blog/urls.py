d961ec9929f6b85e1d5d689388e7f2f4a449d486
Igor Permyakov <imouttasightx@gmail.com>
2020-05-11 19:16:36 +0600

    Added views, templates to delete posts and tags

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetails.as_view(), name='post_details_url'),
     path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url'),
+    path('post/<str:slug>/delete/', PostDelete.as_view(), name='post_delete_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
     path('tag/<str:slug>/', TagDetails.as_view(), name='tag_details_url'),
     path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
+    path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url'),
 ]
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -28,6 +28,12 @@ class PostUpdate(ObjectUpdateMixin, View):
     template = 'blog/post_update.html'
 
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+
+
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
@@ -47,3 +53,9 @@ class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update.html'
+
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
