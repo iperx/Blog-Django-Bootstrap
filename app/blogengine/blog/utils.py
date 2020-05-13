e92d2f6e8069f5136eec5ba293a14578f6d4cfb6
Igor Permyakov <imouttasightx@gmail.com>
2020-05-13 19:24:46 +0600

    Added admin panel

--- app/blogengine/blog/utils.py
+++ app/blogengine/blog/utils.py
@@ -13,7 +13,8 @@ class ObjectDetailsMixin():
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template, context={
-            self.model.__name__.lower(): obj
+            self.model.__name__.lower(): obj,
+            'admin_object': obj,
             })
 
 
