d0592b675668aa3254c203959eddc1f965d244ea
Igor Permyakov <imouttasightx@gmail.com>
2022-06-14 01:02:08 +0600

    Changed function-based views to class-based views for lists of tags and posts, therefore the business logic was completely encapsulated in utils.py.

--- app/blogengine/blog/utils.py
+++ app/blogengine/blog/utils.py
@@ -1,22 +1,112 @@
+from collections import namedtuple
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-from .models import *
-from .forms import *
 
 
-class ObjectDetailsMixin():
+class PaginationMixin:
+    """Adds pagination"""
+
+    pagination = False
+    page_size = 3
+
+    def paginate(self, obj_list, page_number):
+        """
+        Paginate a QuerySet object. Returns context dictionary with keys:
+        page_obj, next_url, prev_url, is_paginated
+        """
+        paginator = Paginator(obj_list, self.page_size)
+        page = paginator.get_page(page_number)
+        is_paginated = page.has_other_pages()
+
+        next_url = prev_url = ''
+        if page.has_next():
+            next_url = 'page={}'.format(page.next_page_number())
+        if page.has_previous():
+            prev_url = 'page={}'.format(page.previous_page_number())
+
+        context = {
+            'page_obj': page,
+            'next_url': next_url,
+            'prev_url': prev_url,
+            'is_paginated': is_paginated,
+        }
+        return context
+
+
+class SearchMixin:
+    """Adds a simple search for model objects"""
+
+    def search(self, search_query):
+        """
+        Look up for particular results according to user search query.
+        Returns QuerySet object
+        """
+        if hasattr(self.model, 'body'):
+            obj_list = self.model.objects.filter(
+                Q(title__icontains=search_query) |
+                Q(body__icontains=search_query)
+            )
+        else:
+            obj_list = self.model.objects.filter(
+                title__icontains=search_query
+                )
+        return obj_list
+
+
+class ObjectListMixin(PaginationMixin, SearchMixin):
+    model = None
+    template = None
+
+    def get(self, request):
+        """
+        Returns rendered page with list of objects depending on search results and pagination
+        """
+        search_query = request.GET.get('search')
+        if search_query:
+            obj_list = self.search(search_query)
+        else:
+            obj_list = self.model.objects.all()
+
+        if self.pagination:
+            page_number = request.GET.get('page', 1)
+            context = self.paginate(obj_list, page_number)
+            context['search'] = search_query
+        else:
+            # namedtuple imitates class django.core.paginator.Page with
+            # 'object_list' field. This is done to maintain common behaviour
+            # across templates, whether the object list is paginated or not.
+            obj_list = namedtuple('PageObjectStub', ['object_list'])(obj_list)
+            context = {
+                'page_obj': obj_list,
+                'is_paginated': False
+            }
+        return render(request, self.template, context)
+
+
+class ObjectDetailsMixin(PaginationMixin):
     model = None
     template = None
 
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
-        return render(request, self.template, context={
-            self.model.__name__.lower(): obj,
-            'admin_object': obj,
-            })
+        model_name = self.model.__name__
+        context = {model_name.lower(): obj, 'admin_object': obj}
+        if model_name != 'Tag':
+            return render(request, self.template, context)
+
+        obj_list = obj.posts.all()
+        if self.pagination:
+            page_number = request.GET.get('page', 1)
+            context.update(self.paginate(obj_list, page_number))
+        else:
+            obj_list = namedtuple('PageObjectStub', ['object_list'])(obj_list)
+            context.update({'page_obj': obj_list, 'is_paginated': False})
+        return render(request, self.template, context)
 
 
 class ObjectCreateMixin(LoginRequiredMixin):
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,51 +1,21 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
 from django.views import View
-from django.core.paginator import Paginator
-from django.db.models import Q
 
-from .models import *
-from .utils import *
-from .forms import *
-
-
-def paginate(request, posts):
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page', 1)
-    page = paginator.get_page(page_number)
-    is_paginated = page.has_other_pages()
-
-    if page.has_next():
-        next_url = 'page={}'.format(page.next_page_number())
-    else:
-        next_url = ''
-    if page.has_previous():
-        prev_url = 'page={}'.format(page.previous_page_number())
-    else:
-        prev_url = ''
-
-    context = {
-        'page_obj': page,
-        'next_url': next_url,
-        'prev_url': prev_url,
-        'is_paginated': is_paginated,
-    }
-    return context
+from .forms import PostForm, TagForm
+from .models import Post, Tag
+from .utils import (
+    ObjectListMixin,
+    ObjectDetailsMixin,
+    ObjectCreateMixin,
+    ObjectUpdateMixin,
+    ObjectDeleteMixin
+)
 
 
-def posts_list(request):
-    search_query = request.GET.get('search', '')
-    if search_query:
-        posts = Post.objects.filter(
-            Q(title__icontains=search_query) | Q(body__icontains=search_query)
-            )
-    else:
-        posts = Post.objects.all()
-
-    context = paginate(request, posts)
-    context['search'] = search_query
-
-    return render(request, 'blog/index.html', context)
+class PostList(ObjectListMixin, View):
+    model = Post
+    template = 'blog/index.html'
+    pagination = True
+    page_size = 3
 
 
 class PostDetails(ObjectDetailsMixin, View):
@@ -70,14 +40,18 @@ class PostDelete(ObjectDeleteMixin, View):
     redirect_url = 'posts_list_url'
 
 
-def tags_list(request):
-    tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
+class TagList(ObjectListMixin, View):
+    model = Tag
+    template = 'blog/tags_list.html'
+    pagination = True
+    page_size = 5
 
 
 class TagDetails(ObjectDetailsMixin, View):
     model = Tag
     template = 'blog/tag_details.html'
+    pagination = True
+    page_size = 3
 
 
 class TagCreate(ObjectCreateMixin, View):
