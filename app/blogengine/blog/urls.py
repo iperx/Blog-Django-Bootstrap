8be63c0c7dcae6bf7e539ba8d22ac68019dad11e
Igor Permyakov <imouttasightx@gmail.com>
2022-06-20 19:26:27 +0600

    Showed particular imports in blog.urls.py

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from .views import *
+from .views import (
+    PostList, PostCreate, PostDetails, PostUpdate, PostDelete,
+    TagList, TagCreate, TagDetails, TagUpdate, TagDelete,
+    SignUpView, SignInView, SignOutView
+)
 
 
 urlpatterns = [
